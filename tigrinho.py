50d0b7fd0bba79e673b9012504bd1038c46b0bf4
luiz <luizgabriel10121998@gmail.com>
2025-05-08 11:32:35 -0300

    tigrinho

--- tigrinho.py
+++ tigrinho.py
@@ -1,8 +1,9 @@
 import random
 import time
 import os
+os.system("cls")
 print("-"*40)
-print("BANANA BET")
+print("      BANANA BET")
 print("-"*40)
 #saca
 #deposita
@@ -21,11 +22,11 @@ macaco = print(r"""
         '._ '-=-' _.'       
            '~---~'          
     """)
-print("-"*40)
 m = 0
 bananas = 0.0
-while m < 2:
-    print(" 1:Depositar \n 2:Jogar \n 3:Sair")
+while m != 2:
+    print("-"*40)
+    print(" 1:Depositar \n 2:Jogar \n 3:Sair \n 4:Sacar")
     print(f"saldo de bananas: {bananas}")
     print("-"*40)
     m = int(input("digite oque deseja fazer: "))
@@ -33,16 +34,30 @@ while m < 2:
     if m == 1:
         bananas = float(input("Quantas bananas você deseja depositar: "))
         print(f"{bananas}")
+    elif m == 4:
+       if bananas == 0:
+            print("você não tem bananas para sacar, por favor deposite bananas para jogar e ganhar mais bananas ;)")
+            time.sleep(1)
+       else:
+        sacado = float(input(f"você tem {bananas} bananas, gostaria de sacar tudo ou só uma parte? Digite quanto você quer sacar: "))
+        print(f"Você quer sacar {sacado} bananas, nossa casa de investimentos pega uma parte dessas bananas para se manter ativo")
+        roubo = sacado*0.25
+        print(f"Você sacou {roubo} bananas")
+        bananas = bananas - sacado
+        time.sleep(1)
 print("*"*40)
 print("HORA DE JOGAR")
 print("*"*40)
 aposta = float(input("Gostaria de apostar quanto? "))
-giros = int(input("Ira girar quantas vezes? "))
+tentativas = bananas/aposta
+
+# giros = int(input("Ira girar quantas vezes? "))
 print("-"*40)
 while giros >= 0:
     time.sleep(1)
     os.system("color 7")
-    if bananas == 0:
+    print(f"Você tem {giros} restantes")
+    if bananas <= 0:
         print("acabou suas bananas coloque mais :)")
         break
     primeiro = random.randint(1,3)
@@ -220,13 +235,13 @@ jgs  /__&gt;     '.       \_ _ _/   ,  '--.
     if terceiro == segundo and segundo == primeiro:
         print("Ganhou")
         bananas = bananas + (0.2*aposta)
-        print(f"{bananas}")
+        print(f"bananas: {bananas}")
         os.system("color 2")
         time.sleep(4)
     else:
         print("Perdeu")
         bananas = bananas - aposta
-        print(f"{bananas}")
+        print(f"bananas: {bananas}")
         os.system("color 4")
         time.sleep(4)
 
